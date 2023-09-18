90e85cfc78ee006db9f974d845ba39e161cc81ea
Aryan mishra <113131432+Aryan-Mac01@users.noreply.github.com>
2023-09-18 10:22:59 +0530

    login page

--- app.js
+++ app.js
@@ -4,17 +4,21 @@ const fs = require('fs');
 const home = fs.readFileSync('home.html');
 const service = fs.readFileSync('service.html');
 const contact = fs.readFileSync('contact.html');
+const login = fs.readFileSync('application.html');
 const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type','text/html');
     res.statusCode=200;
     if(req.url === '/'){
-        res.write(home);
+        res.write(login);
     }
     else if(req.url === '/contact'){
         res.write(contact);
     }
     else if(req.url === '/service'){
         res.write(service);
+    }
+    else if(req.url === '/home'){
+        res.write(home);
     }
     res.end();
 })
